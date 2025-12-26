ac13b3afb31015eeeb45d5d9e48dad5804fdec08
taeyoung0524 <miseoseo1@naver.com>
2025-12-26 16:53:31 +0900

    FEAT : 로그인 API 연동

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,22 +2,74 @@ import React from 'react'
 import "../../assets/sass/section/login/login.scss"
 import arrow from "../../assets/img/arrow.png"
 import Logo from "../../assets/img/login/CLEAR.png"
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [severError, setServerError] = useState('');
+
+  const isFormValid = email.trim() && password.trim();
+
+  const handleLogin = async() => {
+    if(!isFormValid || isSubmitting) return;
+
+    setIsSubmitting(true);
+    setServerError('');
+
+    const payload = {
+      email : email.trim(),
+      password,
+    };
+
+    try{
+      const res = await fetch("http://solserver.store/api/v1/auth/login",{
+        method:"POST",
+        headers:{"Content-Type" : "application/json"},
+        body:JSON.stringify(payload),
+      });
+
+      let dataText = "";
+      try{
+        dataText = await res.text();
+      } catch {}
+
+      if(!res.ok){
+        let msg = "로그인에 실패했어요.";
+        try{
+          const json = JSON.parse(dataText);
+          msg = json?.message||msg;
+        } catch{
+          if(dataText) msg = dataText;
+        } 
+        throw new Error(msg);
+      }
+
+      navigate("/");
+    } catch(err){
+      setServerError(err.message || "오류가 발생했어요.")
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
   return (
     <div className='container login'>
       <div className="header">
-        <button className='arrow'><img src={arrow} alt="" /></button>
+        <button className='arrow' onClick={() => navigate(-1)}><img src={arrow} alt="" /></button>
       </div>
       <div className="middle">
         <img className="logo" src={Logo} alt="" />
         <div className="input_container">
-            <input type="text"  placeholder='이메일'/>
-            <input type="text" placeholder='비밀번호'/>
+            <input type="text"  placeholder='이메일' value = {email} onChange={(e)=> setEmail(e.target.value)}/>
+            <input type="text" placeholder='비밀번호' value = {password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div className="button_container">
-            <button className='login_button'>로그인</button>
-            <button className='signup_button'>회원가입하기</button>
+            <button className='login_button' disabled={!isFormValid || isSubmitting} onClick={handleLogin}>로그인</button>
+            <button className='signup_button' onClick={() => navigate('/signup')}>회원가입하기</button>
         </div>
       </div>
     </div>
